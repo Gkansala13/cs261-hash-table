4c181a10a061eba68b1262df5a4fdd3da34bf4fb
Grayson Kansala <kansalag@oregonstate.edu>
2020-10-18 22:41:17 -0700

    1-8 tests complete, forgot to commit my changes

--- hash_table.py
+++ hash_table.py
@@ -1,9 +1,30 @@
 # HashTable: An unordered key-value data structure providing O(1) store, retrieve
 # search and delete operations.
 # Your implementation should pass the tests in test_hash_table.py.
-# YOUR NAME
+# Grayson Kansala
 
 
 class HashTable:
+    def __init__(self , size = 10):
+        self.size = size
+        self.data= [[] for i in range(self.size)]
+        self.keys=[]
 
-    pass
+    def __setitem__ (self, key, value):
+        self.data=value
+        self.keys=key
+        
+    def __getitem__ (self, key):
+        for i in self.keys:
+            if i == key:
+                return self.data[i]
+
+    def hash(self, value):
+        return hash(value) % self.size
+
+
+
+
+
+
+       
